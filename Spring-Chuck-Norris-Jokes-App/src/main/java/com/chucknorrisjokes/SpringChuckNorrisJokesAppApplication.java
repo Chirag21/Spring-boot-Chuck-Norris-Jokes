570db9c4ec97d8666b67e31bf86eb3a2c0a47c3c
Chirag21 <cp.chirag21@gmail.com>
2021-03-19 20:50:00 +0530

    Initial Commit

---
+++ Spring-Chuck-Norris-Jokes-App/src/main/java/com/chucknorrisjokes/SpringChuckNorrisJokesAppApplication.java
@@ -0,0 +1,13 @@
+package com.chucknorrisjokes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringChuckNorrisJokesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringChuckNorrisJokesAppApplication.class, args);
+	}
+
+}
---
+++ Spring-Chuck-Norris-Jokes-App/src/test/java/com/chucknorrisjokes/SpringChuckNorrisJokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chucknorrisjokes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringChuckNorrisJokesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
