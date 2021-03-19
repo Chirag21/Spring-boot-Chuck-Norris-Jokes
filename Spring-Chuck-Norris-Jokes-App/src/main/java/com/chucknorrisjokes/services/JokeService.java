4159c9258493c2d8ff65c449d2eaeedf4a4d8b0f
Chirag21 <cp.chirag21@gmail.com>
2021-03-19 22:51:39 +0530

    Without dependency injection.

---
+++ Spring-Chuck-Norris-Jokes-App/src/main/java/com/chucknorrisjokes/services/JokeService.java
@@ -0,0 +1,6 @@
+package com.chucknorrisjokes.services;
+
+@FunctionalInterface
+public interface JokeService {
+	String getJoke();
+}
---
+++ Spring-Chuck-Norris-Jokes-App/src/main/java/com/chucknorrisjokes/services/controllers/JokeController.java
@@ -0,0 +1,23 @@
+package com.chucknorrisjokes.services.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.chucknorrisjokes.services.JokeService;
+
+@Controller
+public class JokeController {
+	private final JokeService jokeService;
+
+	public JokeController(JokeService jokeService) {
+		super();
+		this.jokeService = jokeService;
+	}
+	
+	@RequestMapping({"/",""})
+	public String showJoke(Model model) {
+		model.addAttribute("joke", jokeService.getJoke());
+		return "index";
+	}
+}
